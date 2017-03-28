869277090b0fc347fbf581592a8473b6c9318190
wangHao <smile78484@aliyun.com>
2017-03-28 16:41:10 +0800

    create spring cloud demo

---
+++ demo-client/src/main/java/com/idasound/cloud/SpringcloudClientApplication.java
@@ -0,0 +1,16 @@
+package com.idasound.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+
+@SpringBootApplication
+public class SpringcloudClientApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudClientApplication.class, args);
+	}
+
+}
