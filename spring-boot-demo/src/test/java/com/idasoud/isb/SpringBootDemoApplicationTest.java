29f92b690e1e7542c87d18272aafc47b0216f659
wangHao <smile78484@aliyun.com>
2017-03-29 10:44:43 +0800

    add spring boot demo
    添加spring boot  demo

---
+++ spring-boot-demo/src/test/java/com/idasoud/isb/SpringBootDemoApplicationTest.java
@@ -0,0 +1,16 @@
+package com.idasoud.isb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootDemoApplicationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+}
